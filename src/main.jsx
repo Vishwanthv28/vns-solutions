16145fdc884eac7aa97fe61ec086c253dfe7272e
Vishwanthv28 <vishwanthvulli25@gmail.com>
2026-09-13 18:27:35 +0530

    Route concept project pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import Concepts from "./Concepts";
 import "./App.css";
 
+const isConcept = window.location.pathname.startsWith("/concepts/");
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode><App /></StrictMode>
+  <StrictMode>{isConcept ? <Concepts /> : <App />}</StrictMode>
 );
