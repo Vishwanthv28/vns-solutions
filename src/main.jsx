b95ac3f90540f94d61c2591c810607e6f0eaab7c
Vishwanthv28 <vishwanthvulli25@gmail.com>
2026-09-13 18:36:16 +0530

    Load concept styles after main site styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import Concepts from "./Concepts";
 import "./App.css";
+import Concepts from "./Concepts";
 
 const isConcept = window.location.pathname.startsWith("/concepts/");
 
